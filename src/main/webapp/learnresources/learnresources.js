7aee371140b0ecafc827a32948f1634222c15e56
xiongjun <674249291@qq.com>
2016-03-31 18:54:21 +0800

    学习资源的前台

--- src/main/webapp/learnresources/learnresources.js
+++ src/main/webapp/learnresources/learnresources.js
@@ -1,13 +1,26 @@
 define(['toastr', 'bootstrapValidator'], function (toastr) {
 
     function init() {
-        initNoticeList(); // 初始化列表
-        initButton();     // 初始化点击事件
+        //initNoticeList(); // 初始化列表
+        //initButton();     // 初始化点击事件
     }
 
 
 
 
+    function over(){
+        showLink = $(".showLink");
+        shouName = $(".shouName");
+        shouName.css("display","none") ;
+        showLink.css("display","block") ;
+    }
+
+    function out(){
+        showLink = $(".showLink");
+        shouName = $(".shouName");
+        shouName.css("display","block") ;
+        showLink.css("display","none") ;
+    }
 
     //========================
     var learnresources = {};
@@ -16,6 +29,12 @@ define(['toastr', 'bootstrapValidator'], function (toastr) {
     };
     learnresources.removeNoticeModal = function(noticeId) {
         removeNoticeModal(noticeId);
+    };
+    learnresources.out = function(){
+        out();
+    };
+    learnresources.over = function(){
+        over();
     };
     return learnresources;
 });
